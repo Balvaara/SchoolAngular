1e71220156ec15c340a74066085da08a842835dc
Balvaara <lamine.mbaye0104@gmail.com>
2021-08-08 13:53:32 +0000

    pra

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const app=express();
 
 app.use(express.static(__dirname + '/dist/SaAngular'));
 
-app.get('/*', function(req, res) { 
-    res.sendFile('index.html', {root: 'dist/SaAngular/'} 
-  ); 
-  });
+
 
 
 app.listen(process.env.PORT || 8080);
